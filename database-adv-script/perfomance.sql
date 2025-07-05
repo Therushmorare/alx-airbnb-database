c5437a63062ce2ce92b5768855202fee768254fd
Therushmorare <36578137+Therushmorare@users.noreply.github.com>
2025-07-05 16:31:41 +0200

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -17,3 +17,22 @@ FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
 LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+
+EXPLAIN ANALYZE
+SELECT
+  b.booking_id,
+  b.booking_date,
+  u.user_id,
+  u.username,
+  u.email,
+  p.property_id,
+  p.name AS property_name,
+  p.location,
+  pay.payment_id,
+  pay.amount,
+  pay.payment_date,
+  pay.status
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
